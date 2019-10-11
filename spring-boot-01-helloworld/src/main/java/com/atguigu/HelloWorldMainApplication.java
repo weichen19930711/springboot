195aef5a7d5a459b797f68708412149c4995a39c
chenwei26389 <chenwei26389@hundsun.com>
2019-10-11 20:23:58 +0800

    spring-boot-01-helloworld

--- spring-boot-01-helloworld/src/main/java/com/atguigu/HelloWorldMainApplication.java
+++ spring-boot-01-helloworld/src/main/java/com/atguigu/HelloWorldMainApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @ 来标注一个主程序类，说明这是一个springBoot 应用
+ *  Spring Boot应用标注在某个类上说明这个类是SpringBoot的主配置类，SpringBoot就应该运行这个类的main方法来启动SpringBoot应用
  */
 @SpringBootApplication
 public class HelloWorldMainApplication {
--- spring-boot-01-helloworld/src/main/java/com/atguigu/controller/HelloController.java
+++ spring-boot-01-helloworld/src/main/java/com/atguigu/controller/HelloController.java
@@ -1,15 +1,13 @@
 package com.atguigu.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
-  @ResponseBody
   @RequestMapping("/hello")
   public String sayHello() {
-    return "hello";
+    return "hello1";
   }
 }
