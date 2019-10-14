c476d23f90456720b58506e0cea7f8a5a44a20ed
chenwei26389 <chenwei26389@hundsun.com>
2019-10-14 21:52:38 +0800

    20191014

--- spring-boot-03-logging/src/test/java/com/atguigu/springboot/SpringBoot03LoggingApplicationTests.java
+++ spring-boot-03-logging/src/test/java/com/atguigu/springboot/SpringBoot03LoggingApplicationTests.java
@@ -14,11 +14,13 @@ public class SpringBoot03LoggingApplicationTests {
 	private Logger logger = LoggerFactory.getLogger(getClass());
 	@Test
 	public void contextLoads() {
-		logger.trace("trace...");
-		logger.debug("debug...");
-		logger.info("info...");
-		logger.warn("warn...");
-		logger.error("error...");
+		// 日志的级别，由低到高
+    // 可以指定输出的日志级别 ，比如指定warn，就只输出warn和error级别 的日志
+		logger.trace("trace...");// 跟踪
+		logger.debug("debug...");// 调试
+		logger.info("info...");//
+		logger.warn("warn...");// 警告
+		logger.error("error...");// 错误
 	}
 
 }
