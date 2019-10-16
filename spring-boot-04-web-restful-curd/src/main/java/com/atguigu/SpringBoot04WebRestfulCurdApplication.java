3ee836e2ae3718cd61681e10d11898ed37401165
chenwei26389 <chenwei26389@hundsun.com>
2019-10-16 20:16:53 +0800

    first commit

---
+++ spring-boot-04-web-restful-curd/src/main/java/com/atguigu/SpringBoot04WebRestfulCurdApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot04WebRestfulCurdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot04WebRestfulCurdApplication.class, args);
+	}
+
+}
